3ed323a073fb498c8a27304d63694b98e542bab4
Rakesh Kelkar <rakelkar@gmail.com>
2018-02-08 05:29:49 +0000

    add flask website

---
+++ site/view.py
@@ -0,0 +1,12 @@
+#!/usr/bin python
+import sys
+
+from flask import *
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('hello.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
